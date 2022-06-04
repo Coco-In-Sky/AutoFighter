24ebf297dd8a4036282f34e7664f82c8727022c4
icear <icear@users.noreply.github.com>
2022-06-04 14:09:01 +0800

    master branch clean PRTS relate file

--- utils/controller/__init__.py
+++
@@ -1,3 +0,0 @@
-# __all__ = ["Controller", "ADBController", "WindowsAPIController"]
-
-CONTEXT_KEY_CONTROLLER = 'context_key_controller'
--- utils/ocr/__init__.py
+++
@@ -1,1 +0,0 @@
-CONTEXT_KEY_OCR_RESULT = 'context_key_ocr_result'
